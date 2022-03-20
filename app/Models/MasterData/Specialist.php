b57a3e8497bfaa738a877626b1393777c773246e
arindra97 <ar28297@gmail.com>
2022-03-20 14:14:09 +0700

    Make a Model

---
+++ app/Models/MasterData/Specialist.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models\MasterData;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Specialist extends Model
+{
+    // use HasFactory;
+    use SoftDeletes;
+
+    // declare table
+    public $table = 'specialist';
+
+    // this field must type date yyyy-mm-dd hh:mm:ss
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    // declare fillable
+    protected $fillable = [
+        'name',
+        'price',
+        'updated_at',
+        'deleted_at',
+    ];
+}
