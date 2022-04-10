052f68f065a8666d1f7e78ebc96328aeb23b557b
arindra97 <ar28297@gmail.com>
2022-04-10 16:35:07 +0700

    update menu

--- resources/views/components/backsite/menu.blade.php
+++ resources/views/components/backsite/menu.blade.php
@@ -3,41 +3,25 @@
 <div class="main-menu menu-fixed menu-light menu-accordion    menu-shadow " data-scroll-to-active="true">
     <div class="main-menu-content">
         <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-            <li class=" nav-item"><a href="index.html"><i class="la la-home"></i><span class="menu-title" data-i18n="Dashboard">Dashboard</span><span class="badge badge badge-info badge-pill float-right mr-2">3</span></a>
-                <ul class="menu-content">
+            <li class=" nav-item"><a href="{{ route('backsite.dashboard.index') }}"><i class="la la-home"></i><span class="menu-title" data-i18n="Dashboard">Dashboard</span><span class="badge badge badge-info badge-pill float-right mr-2">3</span></a>
+                {{-- <ul class="menu-content">
                     <li class="active"><a class="menu-item" href="dashboard-ecommerce.html"><i></i><span data-i18n="eCommerce">eCommerce</span></a>
                     </li>
                     <li><a class="menu-item" href="dashboard-crypto.html"><i></i><span data-i18n="Crypto">Crypto</span></a>
                     </li>
                     <li><a class="menu-item" href="dashboard-sales.html"><i></i><span data-i18n="Sales">Sales</span></a>
                     </li>
-                </ul>
+                </ul> --}}
             </li>
-            <li class=" nav-item"><a href="#"><i class="la la-television"></i><span class="menu-title" data-i18n="Templates">Templates</span></a>
+            <li class=" nav-item"><a href="#"><i class="la la-television"></i><span class="menu-title" data-i18n="Management-Access">Management Access</span></a>
                 <ul class="menu-content">
-                    <li><a class="menu-item" href="#"><i></i><span data-i18n="Vertical">Vertical</span></a>
-                        <ul class="menu-content">
-                            <li><a class="menu-item" href="../vertical-menu-template"><i></i><span data-i18n="Classic Menu">Classic Menu</span></a>
-                            </li>
-                            <li><a class="menu-item" href="../vertical-modern-menu-template"><i></i><span data-i18n="Modern Menu">Modern Menu</span></a>
-                            </li>
-                            <li><a class="menu-item" href="../vertical-collapsed-menu-template"><i></i><span data-i18n="Collapsed Menu">Collapsed Menu</span></a>
-                            </li>
-                            <li><a class="menu-item" href="../vertical-compact-menu-template"><i></i><span data-i18n="Compact Menu">Compact Menu</span></a>
-                            </li>
-                            <li><a class="menu-item" href="../vertical-content-menu-template"><i></i><span data-i18n="Content Menu">Content Menu</span></a>
-                            </li>
-                            <li><a class="menu-item" href="../vertical-overlay-menu-template"><i></i><span data-i18n="Overlay Menu">Overlay Menu</span></a>
-                            </li>
-                        </ul>
+                    <li class="{{ request()->is('/') ? "active" : "" }}"><a class="menu-item" href="{{ route('backsite.permission.index') }}"><i></i><span data-i18n="Permission">Permission</span></a>
                     </li>
-                    <li><a class="menu-item" href="#"><i></i><span data-i18n="Horizontal">Horizontal</span></a>
-                        <ul class="menu-content">
-                            <li><a class="menu-item" href="../horizontal-menu-template"><i></i><span data-i18n="Classic">Classic</span></a>
-                            </li>
-                            <li><a class="menu-item" href="../horizontal-menu-template-nav"><i></i><span data-i18n="Full Width">Full Width</span></a>
-                            </li>
-                        </ul>
+                    <li class="{{ request()->is('/') ? "active" : "" }}"><a class="menu-item" href="{{ route('backsite.role.index') }}"><i></i><span data-i18n="Role">Role</span></a>
+                    </li>
+                    <li class="{{ request()->is('/') ? "active" : "" }}"><a class="menu-item" href="{{ route('backsite.type-user.index') }}"><i></i><span data-i18n="Type-User">Type User</span></a>
+                    </li>
+                    <li class="{{ request()->is('/') ? "active" : "" }}"><a class="menu-item" href="{{ route('backsite.user.index') }}"><i></i><span data-i18n="User">User</span></a>
                     </li>
                 </ul>
             </li>
